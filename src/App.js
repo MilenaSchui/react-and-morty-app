248ccf1cfaca4742bd1dc70d11aef43f2934e441
Milena <114745314+MilenaSchui@users.noreply.github.com>
2022-11-10 18:51:07 +0100

    added style components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
+import styled from 'styled-components';
+// import Card from './Components/Card/Card';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Das ist ein Header</h1>       
-      </header>
-      <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"/>
-      <h2>Morty Smith</h2>
-      <footer>
-        <nav>
-          <p>Placeholder</p>
-          <p>Placeholder</p>
-          <p>Placeholder</p>
-          <p>Placeholder</p>
-        </nav>
-      </footer>
+      <Header>React and Morty App</Header>     
+      <Main>
+        <Card>
+        <Image src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"/>
+        <h2>Morty Smith</h2>
+        </Card>
+      </Main>
+      <NavBar>
+        <p>Placeholder</p>
+        <p>Placeholder</p>
+        <p>Placeholder</p>
+        <p>Placeholder</p>
+      </NavBar>
     </div>
   );
 }
@@ -23,3 +25,45 @@ function App() {
 
 
 export default App;
+
+const Header = styled.h1`
+  display: flex;
+  justify-content: center;
+  margin-top: 0;  
+  height: 50px;
+  width: 100%;
+  position: fixed;
+  text-transform: lowercase;
+  color: #FFFDED;
+  background: #140F2D;
+`;
+
+const Main = styled.div`
+  background: #717C89;
+  height: 1500px;
+`;
+
+const Card = styled.div`
+  border: 1px solid black;
+  width: 80%;
+  margin: 10px auto;
+  max-width: 400px;
+  overflow: hidden;
+  color: #FFFDED;
+  background: #FFB17A;
+`;
+
+const Image = styled.img`
+  width: 100%; 
+`;
+
+const NavBar = styled.footer`
+display: flex;
+justify-content: space-around;  
+height: 50px;
+width: 100%;
+position: fixed;
+bottom: 0;
+color: #FFFDED;
+background: #140F2D;
+`;
