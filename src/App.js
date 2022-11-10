9a6594b1d7f264ccfd03640d60931fd73c263828
Milena <114745314+MilenaSchui@users.noreply.github.com>
2022-11-10 16:09:19 +0100

    did next TODOs

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>Das ist ein Header</h1>       
+      </header>
+      <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"/>
+      <h2>Morty Smith</h2>
+      <footer>
+        <nav>
+          <p>Placeholder</p>
+          <p>Placeholder</p>
+          <p>Placeholder</p>
+          <p>Placeholder</p>
+        </nav>
+      </footer>
     </div>
   );
 }
+
+
 
 export default App;
